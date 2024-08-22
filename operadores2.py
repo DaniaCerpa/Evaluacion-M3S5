032ee1182fe6b3974567199e76685511be63ace6
DaniaCerpa <dania.cerpa@gmail.com>
2024-08-22 19:47:34 -0400

    Segundo commit

--- operadores2.py
+++ operadores2.py
@@ -1,11 +1,11 @@
-palabra = "paralelepípedo"
+palabra = "CalcetÍN SUcio"
 indice = 0
 
-for var in palabra:
+for letra in palabra.lower():
     indice +=1
-    if var == "a" or var == "e" or var == "i" or var == "í" or var == "o" or var == "u":
+    if letra == "a" or letra == "á" or letra == "e" or letra == "é" or  letra == "i" or letra == "í" or letra == "o" or letra == "ó" or letra == "u" or letra == "ú":
         continue 
     else:
-        print (f"Posición {indice}: {var}")
+        print (f"Posición {indice}: {letra}")
 
 
